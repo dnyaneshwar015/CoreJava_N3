18fb48e394a3fbc82723b16b0e6ed2ec0a12bd26
Student <Student@IOE-AIDS-193>
2023-02-01 13:58:24 +0530

    First Commit

---
+++ dayone/src/org/tnsif/intro/SimpleJavaProgramDayOne.java
@@ -0,0 +1,10 @@
+package org.tnsif.intro;
+
+public class SimpleJavaProgramDayOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello World");
+	}
+
+}
