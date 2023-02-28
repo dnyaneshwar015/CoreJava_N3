92b01f766d7901250622c5e00f1ffa07234f0e77
om <om@DESKTOP-I236LMB>
2023-02-28 07:40:10 -0800

    sixteenth commit

---
+++ daysixteen/src/org/tnsif/junit/Test.java
@@ -0,0 +1,12 @@
+package org.tnsif.junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Test {
+
+	@org.junit.jupiter.api.Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
