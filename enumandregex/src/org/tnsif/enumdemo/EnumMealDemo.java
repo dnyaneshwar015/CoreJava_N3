e111ece6f8c2c89c793254340fd4fc1de9e5ba26
om <om@DESKTOP-I236LMB>
2023-03-02 11:51:12 -0800

    fifteenth commit

---
+++ enumandregex/src/org/tnsif/enumdemo/EnumMealDemo.java
@@ -0,0 +1,14 @@
+package org.tnsif.enumdemo;
+
+public class EnumMealDemo {
+
+	public static void main(String[] args) {
+		//we cannot create the object for enum
+		//Meal m=new Meal();
+		for(Meal m:Meal.values())
+		{
+			System.out.println(m+" "+m.getValue());
+		}
+
+	}
+}
